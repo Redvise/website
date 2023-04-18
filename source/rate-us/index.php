30c77b44787d23a8f2698db951c1c7b6d937dd24
Adnan RIHAN <adnan@rihan.fr>
2023-04-18 19:23:03 +0200

    Rate-Us page, redirects to Google Business

---
+++ source/rate-us/index.php
@@ -0,0 +1,17 @@
+<?php header('Location: https://g.page/r/CanCQVsq0LnzEB0/review'); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Redirecting...</title>
+  <link rel="canonical" href="https://g.page/r/CanCQVsq0LnzEB0/review">
+  <meta http-equiv="refresh" content="0;url=https://g.page/r/CanCQVsq0LnzEB0/review">
+</head>
+<body>
+  <h1>Redirecting...</h1>
+  <a href="https://g.page/r/CanCQVsq0LnzEB0/review">Click here if you are not redirected.<a>
+
+    <script>window.location='https://g.page/r/CanCQVsq0LnzEB0/review'</script>
+</body>
+</html>
